d589178841de88e156525d499ca8616410132030
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2022-12-21 20:50:27 +0300

    Update custome_list_tile.dart

--- lib/presentation/widgets/custome_list_tile.dart
+++ lib/presentation/widgets/custome_list_tile.dart
@@ -9,21 +9,28 @@ Widget customeListTile({
     child: Card(
       elevation: 2,
       child: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: MediaQuery.of(context).size.height * 0.02,
-          horizontal: MediaQuery.of(context).size.width * 0.02,
-        ),
-        color: Colors.red,
-        child: Row(
-          children: [
-            Columm(
-              children: [
-                
-              ]
-            ),
-          ],
-        )
-      ),
+          padding: EdgeInsets.symmetric(
+            vertical: MediaQuery.of(context).size.height * 0.01,
+            horizontal: MediaQuery.of(context).size.width * 0.02,
+          ),
+          color: Colors.red,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    width: MediaQuery.of(context).size.height * 0.15,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
     ),
   );
 }
