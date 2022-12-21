8ce47f2614cfc52facfd0eb6d285802631978bd4
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2022-12-21 21:44:55 +0300

    color

---
+++ lib/presentation/my_colors/color.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color darkColor = Colors.black;
+const Color myYellow = Color.fromARGB(255, 220, 95, 0);
--- lib/presentation/widgets/custome_list_tile.dart
+++ lib/presentation/widgets/custome_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mucic_store/presentation/my_colors/color.dart';
 
 Widget customeListTile({
   required BuildContext context,
@@ -7,30 +8,63 @@ Widget customeListTile({
     height: MediaQuery.of(context).size.height * 0.2,
     width: MediaQuery.of(context).size.width,
     child: Card(
-      elevation: 2,
+      elevation: 3,
       child: Container(
-          padding: EdgeInsets.symmetric(
-            vertical: MediaQuery.of(context).size.height * 0.01,
-            horizontal: MediaQuery.of(context).size.width * 0.02,
-          ),
-          color: Colors.red,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Container(
+        padding: EdgeInsets.symmetric(
+          vertical: MediaQuery.of(context).size.height * 0.01,
+          horizontal: MediaQuery.of(context).size.width * 0.02,
+        ),
+        color: darkColor,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Container(
                     height: MediaQuery.of(context).size.height * 0.15,
                     width: MediaQuery.of(context).size.height * 0.15,
                     decoration: const BoxDecoration(
                       shape: BoxShape.circle,
                       color: Colors.white,
                     ),
-                  ),
-                ],
-              ),
-            ],
-          )),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.play_arrow_sharp,
+                      ),
+                    )),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.02,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "The Title of The song",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text("sub title of the song...")
+                  ],
+                )
+              ],
+            ),
+            Row(
+              children: [
+                const Text("5:20"),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_vert),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     ),
   );
 }
